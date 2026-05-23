ebfb725b7247832fee3e4bc0344b8cc3666323fb
iniyanfriend77-glitch <iniyanfriend77@gmail.com>
2026-05-23 21:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (71.04%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i=m-1,j=n-1,k=m+n-1;
+    while(i!=-1||j!=-1)
+    {
+        if(i>=0&&j>=0&&nums1[i]>=nums2[j])
+        {
+            nums1[k]=nums1[i];
+            i--;
+            k--;
+        }
+        else if(i>=0&&j>=0&&nums1[i]<nums2[j])
+        {
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
+        while(i==-1&&j>=0)
+        {
+            nums1[k--]=nums2[j--];
+        }
+        while(j==-1&&i>=0)
+        {
+            nums1[k--]=nums1[i--];
+        }
+    }
+    }
+};
