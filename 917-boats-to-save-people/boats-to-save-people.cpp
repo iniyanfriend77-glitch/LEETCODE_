c201cb8ee028e5d30aaba7fd18e155f3cd3984cf
iniyanfriend77-glitch <iniyanfriend77@gmail.com>
2026-05-23 21:36:10 +0530

    Time: 15 ms (73.75%) | Memory: 45.9 MB (5.25%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -3,21 +3,21 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
         int count=0;
-       int i=0,j=people.size()-1;
-       while(j>=i)
-       {
-        if(people[j]+people[i]<=limit)
+        int i=0,j=people.size()-1;
+        while(j>=i)
         {
-            count++;
-            j--;
-            i++;
+            if(people[i]+people[j]<=limit)
+            {
+                i++;
+                j--;
+                count++;
+            }
+            else
+            {
+                count++;
+                j--;
+            }
         }
-        else 
-        {
-            count++;
-            j--;
-        }
-       }
         return count;
     }
 };
